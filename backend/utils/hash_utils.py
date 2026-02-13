d7deedd209ad4daa922afac5b57a920eec89cc93
AleenaFernz <aleena.fernz@gmail.com>
2026-02-14 02:29:21 +0530

    correct the hash bytes

--- backend/utils/hash_utils.py
+++ backend/utils/hash_utils.py
@@ -1,11 +1,17 @@
-import hashlib #built-in cryptographic library
+import hashlib  # built-in cryptographic library
 
-def generate_sha256(file_bytes: bytes) -> str:    #function accepts raw file bytes
+
+def generate_sha256(file_bytes: bytes) -> str:
     """
     Generates SHA-256 hash of uploaded file bytes.
-    Returns hexadecimal string.
+    Returns 0x-prefixed hexadecimal string
+    compatible with Solidity bytes32.
     """
 
     sha256 = hashlib.sha256()
-    sha256.update(file_bytes)  #feeds file content into hashing algorithm
-    return sha256.hexdigest()
+    sha256.update(file_bytes)  # feeds file content into hashing algorithm
+
+    hash_hex = sha256.hexdigest()
+
+    # Prefix with 0x for Solidity compatibility
+    return "0x" + hash_hex
